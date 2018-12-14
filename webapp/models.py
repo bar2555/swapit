86413396e5b10786efe3ce4df69228702a8145c6
Rob <bar2555@gmx.com>
2018-12-14 20:54:59 +0000

    commit first migration

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+# define a user model
+# TODO
+
+# define item model
+class Item(models.Model):
+    item_name = models.CharField(max_length=50)
+    item_description = models.CharField(max_length=200)
+    date_posted = models.DateTimeField()
